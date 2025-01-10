80cc0b270ff9284d03c37890a14a4a6f00675105
Charlotte Burce <kmlcburce@gmail.com>
2025-01-10 14:22:34 +0800

    views

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/auth/login');
 });
 
 Route::get('/dashboard', function () {
@@ -17,8 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::middleware(['auth'])->group(function () {
+
     Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
     Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
     Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
@@ -26,4 +25,12 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
     Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
 });
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+//     Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+//     Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+//     Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+//     Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+//     Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+// });
 require __DIR__.'/auth.php';
